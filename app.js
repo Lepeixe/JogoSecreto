1ac92616e3c861e8d2d818e342fd67e0bd1f6164
Leandro Peixinho <leandropeixinho01@gmail.com>
2024-12-11 21:48:17 -0300

    Atualizar o app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ exibirMensagemInicial();
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.1});
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
     
     // (Código abaixo da Web Speech API:)
 //    if ('speechSynthesis' in window) {
